eaba71fbede45e8589a6b8bca4504e11700dba36
guayom <guayo.mena@gmail.com>
2017-05-25 10:51:06 -0600

    Arreglos para mobile Menú y layout de tarjetas

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,16 @@
-// This is where it all goes :)
+$(document).ready(function(){
+  $('.tb-megamenu-item.level-1').hover(function(){
+      $(this).find('.dropdown-menu').css('display', 'block');
+      }, function(){
+      $(this).find('.dropdown-menu').css('display', 'none');
+  });
+
+  $('.head-country-selector').click(function(){
+    $('.head-side-menu .head-countries').toggleClass('show');
+  });
+
+  $('.toggle-topbar a').click(function(){
+    console.log('hola');
+    $('.top-bar').toggleClass('expanded');
+  });
+});
