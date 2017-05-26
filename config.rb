d578e3ce373c951248ad5d687a3f5f59805aa9bd
guayom <guayo.mena@gmail.com>
2017-05-26 09:49:44 -0600

    Limpié el repo para original

--- config.rb
+++ config.rb
@@ -9,11 +9,6 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-data.tarjetas.productos.each do |producto|
-  proxy "/revision/#{producto[1]['titulo'].parameterize}.html", "/revision/template.html", :locals => { :producto => producto[1], :titulo => producto[1]['titulo']}, :ignore => true
-  proxy "/es-cr/tarjetas-credito/#{producto[1]['titulo'].parameterize}.html", "/card-template.html", :locals => { :producto => producto[1], :titulo => producto[1]['titulo']}, :ignore => true
-end
-
 # General configuration
 activate :dotenv, env: '.env'
 activate :contentful do |f|
