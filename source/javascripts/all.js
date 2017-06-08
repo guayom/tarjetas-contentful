ab53ed3bf9e15bbb6703dcbe8b35a9453ed61992
guayom <guayo.mena@gmail.com>
2017-06-08 10:42:45 -0600

    Animaciones del header El título se hace más pequeño El subtítulo desaparece Los paddings se reducen

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
 
+  //inicializar filtros. Hace que sólo se muestre la fila de tarjetas que tiene la categoría "ninguna"
+  $('*[data-category="ninguna"]').show();
+
+  //Funciones para los filtros
+  //Esta función hace que se le quite la clase active a todos los filtros
   function unselectAllFilters() {
     $('.filter-item').removeClass('active');
   }
 
+  //Esta función hace que se muestre sólo la fila de tarjetas seleccionada en los filtros
   function filtrarTarjetas (category) {
     $('.card-row').hide();
     $('*[data-category="'+ category +'"]').show();
+  }
+
+  function animarHeader () {
+    $('.filters-container').removeClass('inicial');
   }
 
   $('.filter-item').click(function(){
@@ -14,8 +24,7 @@ $(document).ready(function(){
     unselectAllFilters();
     $(this).addClass('active');
     filtrarTarjetas (category);
+    animarHeader();
   });
 
-  //inicializar filtros
-  $('*[data-category="ninguna"]').show();
 });
