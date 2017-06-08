148c1a28f1ad502793ebd09877f4bc1e3bc83c75
guayom <guayo.mena@gmail.com>
2017-06-08 09:53:05 -0600

    Creé los filtros para las tarjetas

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
 
   function unselectAllFilters() {
-    console.log('Des - seleccionando filtros')
     $('.filter-item').removeClass('active');
   }
 
+  function filtrarTarjetas (category) {
+    $('.card-row').hide();
+    $('*[data-category="'+ category +'"]').show();
+  }
+
   $('.filter-item').click(function(){
+    var category = $(this).data('category');
     unselectAllFilters();
     $(this).addClass('active');
+    filtrarTarjetas (category);
   });
+
+  //inicializar filtros
+  $('*[data-category="ninguna"]').show();
 });
