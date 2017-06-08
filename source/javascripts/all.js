fd4c8a71a75ebd62e046a3d6b464628b862a127e
guayom <guayo.mena@gmail.com>
2017-06-08 09:42:05 -0600

    Agregué estado activo a los filtros

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,25 +1,12 @@
 $(document).ready(function(){
-  $('.tb-megamenu-item.level-1').hover(function(){
-      $(this).find('.dropdown-menu').css('display', 'block');
-      $(this).find('.dropdown-menu .dropdown-menu').css('display', 'none');
-      }, function(){
-      $(this).find('.dropdown-menu').css('display', 'none');
-  });
 
-  $('.head-country-selector').click(function(){
-    $('.head-side-menu .head-countries').toggleClass('show');
-  });
+  function unselectAllFilters() {
+    console.log('Des - seleccionando filtros')
+    $('.filter-item').removeClass('active');
+  }
 
-  $('.toggle-topbar a').click(function(){
-    $('.top-bar').toggleClass('expanded');
-  });
-
-  $('.header-select-country').change(function(){
-    window.location.replace("https://www.baccredomatic.com/" + $(this).val())
-  });
-
-  $('.footer-list-head').click(function(){
-    $('.footer-list').hide();
-    $(this).siblings().toggle();
+  $('.filter-item').click(function(){
+    unselectAllFilters();
+    $(this).addClass('active');
   });
 });
