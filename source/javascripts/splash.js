263ba71021e759a4a547b2cb5ea90961969c0586
guayom <guayo.mena@gmail.com>
2017-06-16 00:43:45 -0600

    Trabajo en header Hice la animación básica para el form de búsqueda Hice estilos básicos Los eiconos son puro css :)

--- source/javascripts/splash.js
+++ source/javascripts/splash.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
 	$('.hamburger').click(function(){
 		$(this).toggleClass('open');
 	});
+	$('.search').click(function(){
+		$('.search-form').slideToggle();
+	});
 });
