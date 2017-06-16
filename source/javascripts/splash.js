516468896a636369e3c5617ffe2cec8cdf084d3e
guayom <guayo.mena@gmail.com>
2017-06-16 14:04:04 -0600

    Terminé versión móvil

--- source/javascripts/splash.js
+++ source/javascripts/splash.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
 	$('.hamburger').click(function(){
 		$(this).toggleClass('open');
+		$('.hamburger-container').toggleClass('open');
+		$('.offcanvas').toggleClass('shown');
 	});
 	$('.search').click(function(){
 		$('.search-form').slideToggle();
+	});
+
+	//Animación para el botón de ver más productoa
+	$(".explora").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+		$(this).toggle();
+	});
+
+	//Quitar el botón de scroll si ya el usuario está abajo
+	$(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() == $(document).height()) {
+       $(".explora").hide();
+   }
 	});
 });
