18ca58c3f9bbc65a6ca5f7a4d6919d525c7d4015
guayom <guayo.mena@gmail.com>
2017-06-16 20:08:05 -0600

    Arreglé el header para desktop

--- source/javascripts/splash.js
+++ source/javascripts/splash.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
+
 	$('.hamburger').click(function(){
 		$(this).toggleClass('open');
 		$('.hamburger-container').toggleClass('open');
 		$('.offcanvas').toggleClass('shown');
 	});
+
+	$('#explora-mas-header-btn').click(function(){
+		$('.hamburger').click();
+	});
+
 	$('.search').click(function(){
 		$('.search-form').slideToggle();
 	});
 
 	//Animación para el botón de ver más productoa
-	$(".explora").click(function(e) {
+	$("#explora-btn-footer").click(function(e) {
     e.preventDefault();
     var section = $(this).attr("href");
     $("html, body").animate({
@@ -21,7 +27,7 @@ $(document).ready(function(){
 	//Quitar el botón de scroll si ya el usuario está abajo
 	$(window).scroll(function() {
    if($(window).scrollTop() + $(window).height() == $(document).height()) {
-       $(".explora").hide();
+       $("#explora-btn-footer").hide();
    }
 	});
 });
